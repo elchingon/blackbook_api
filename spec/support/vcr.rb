4fdb7eb701f5f656c7f35d894688e70a5f45c564
Alexi Carey <ciegoproductions@gmail.com>
2014-02-11 09:58:09 -0700

    Add VCR allow connections when no cassette option

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,6 +2,7 @@ require 'vcr'
 
 VCR.configure do |c|
   c.cassette_library_dir = "spec/cassettes"
+  c.allow_http_connections_when_no_cassette = true
   c.hook_into :webmock
   c.configure_rspec_metadata!
 end
