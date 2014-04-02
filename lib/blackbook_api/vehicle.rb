9491b88aa97c93a879a3090d45df883f993b38ea
Alexi Carey <ciegoproductions@gmail.com>
2014-04-01 18:07:08 -0600

    Update validation for nil values

--- lib/blackbook_api/vehicle.rb
+++ lib/blackbook_api/vehicle.rb
@@ -135,8 +135,8 @@ module BlackbookApi
         vehicle.wheel_base = data["wheel_base"]
         vehicle.tire_size = data["tire_size"]
         vehicle.weight = data["gvw"]
-        vehicle.payload_cap = data["payload_cap"].gsub(",","")
-        vehicle.tow_cap = data["tow_cap"].gsub(",","")
+        vehicle.payload_cap = (!data["payload_cap"].nil? ? data["payload_cap"].gsub(",","") :"")
+        vehicle.tow_cap = (!data["tow_cap"].nil? ? data["tow_cap"].gsub(",","") : "")
         vehicle.seat_cap = data["seat_cap"]
         vehicle.seats = data["seats"]
         vehicle.fuel_cap = data["fuel_cap"]
